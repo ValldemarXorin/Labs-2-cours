428036cd48339e42f583b68c94a7a121a7ea4ed6
ValldemarXorin <vova3089927@gmail.com>
2024-10-25 09:56:29 +0300

    -3

--- source/models/Movie.cpp
+++ source/models/Movie.cpp
@@ -15,32 +15,3 @@ const std::string &Movie::get_age_limit() const { return age_limit; }
 
 int Movie::get_year() const { return year; }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//int Movie::get_Genre_id() const { return Genre_id; }
-//int Movie::get_Human_id() const { return Human_id; }
-//int Movie::get_Rate_id() const { return Rate_id; }
-//int Movie::get_Money_id() const { return Money_id; }
-//int Movie::get_Links_id() const { return Links_id; }
-
-// мб тоже переделать
-//void Movie::display() const {
-//    std::cout << "Title: " << title << ", Year: " << year << ", Genre: "
-//              << genre << ", Director: " << director << ", Rating: " << rating << std::endl;
-//}
-
-
-
-// всё что ниже пересмотреть и переделать
-
